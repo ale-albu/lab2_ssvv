85eab11c4768de203f8bb297065c5660a1fb25ec
Alexandra Albu <alexandralbu14@gmail.com>
2018-03-27 13:34:24 +0300

    Fix files 3.0

--- src/test/java/ssvv/repository/StudentRepositoryTest.java
+++ src/test/java/ssvv/repository/StudentRepositoryTest.java
@@ -12,21 +12,4 @@ public class StudentRepositoryTest {
     StudentRepository repository;
     Controller ctrl;
 
-    @org.junit.Test
-    public void findAll() throws Exception {
-        repository = new StudentRepository("src/test/java/ssvv/students.txt");
-        int nr = repository.findAll().size();
-        ctrl = new Controller(repository, null, null);
-        ctrl.saveStudent(new Student(1, "a b", 200));
-
-        assertEquals(nr+1, repository.findAll().size());
-    }
-
-    @org.junit.Test
-    public void findAllStudentsEmptyList() throws Exception {
-        repository = new StudentRepository("src/test/java/ssvv/student.txt");
-        int nr = repository.findAll().size();
-        assertEquals(nr, 0);
-    }
-
 }
